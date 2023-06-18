0570dde06f499b7fc16bdcca7e7a3aee9c07ff7e
Amollerup <amy.mollerup@gmail.com>
2023-06-18 01:57:34 -0600

    Fixed nullability

--- initDB.sql
+++ initDB.sql
@@ -4,25 +4,24 @@ DROP TABLE IF EXISTS character;
 DROP TABLE IF EXISTS profession;
 DROP TABLE IF EXISTS user;
 
-
 CREATE TABLE user (
   user_id INTEGER PRIMARY KEY,
-  username VARCHAR
+  username VARCHAR NOT NULL
 );
 
 CREATE TABLE profession (
   profession_id INTEGER PRIMARY KEY,
-  profession_name VARCHAR
+  profession_name VARCHAR NOT NULL
 );
 
 CREATE TABLE character (
   character_id INTEGER PRIMARY KEY,
-  user_id INTEGER,
-  character_name VARCHAR,
-  profession_1 INTEGER,
+  user_id INTEGER NOT NULL,
+  character_name VARCHAR NOT NULL,
+  profession_1 INTEGER NOT NULL,
   profession_2 INTEGER,
-  created_on DATE,
-  last_modified DATE,
+  created_on DATE NOT NULL,
+  last_modified DATE NOT NULL,
   FOREIGN KEY(user_id) REFERENCES user(user_id),
   FOREIGN KEY(profession_1) REFERENCES profession(profession_id),
   FOREIGN KEY(profession_2) REFERENCES profession(profession_id)
@@ -30,12 +29,12 @@ CREATE TABLE character (
 
 CREATE TABLE recipe (
   recipe_id INTEGER PRIMARY KEY,
-  recipe_name VARCHAR
+  recipe_name VARCHAR NOT NULL
 );
 
 CREATE TABLE character_recipe (
-  character_id INTEGER,
-  recipe_id INTEGER,
+  character_id INTEGER NOT NULL,
+  recipe_id INTEGER NOT NULL,
   PRIMARY KEY(character_id, recipe_id),
   FOREIGN KEY(character_id) REFERENCES character(character_id),
   FOREIGN KEY(recipe_id) REFERENCES recipe(recipe_id)
